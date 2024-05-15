1a1c3e4d887ecd1b740a5ea502e285c277b7bc63
Maria Rosete <marlarosete89@gmail.com>
2024-05-16 00:19:59 +0200

    version1errormapper

--- pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/servicios/UsuarioMapper.java
+++ pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/servicios/UsuarioMapper.java
@@ -2,31 +2,35 @@ package com.dawes.pdaw_Maria_Rosete.servicios;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.dawes.pdaw_Maria_Rosete.dto.UsuarioDTO;
-import com.dawes.pdaw_Maria_Rosete.modelo.NombreRol;
-import com.dawes.pdaw_Maria_Rosete.modelo.RolVO;
 import com.dawes.pdaw_Maria_Rosete.modelo.UsuarioVO;
 
+/**
+ * Interfaz que define métodos para mapear entre objetos DTO y entidades de Usuario.
+ * 
+ * @author Maria Rosete
+ */
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    public static final ServicioRol servicioRol = null;
-
-    @Mapping(target = "clave", ignore = true)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "token", ignore = true)
+    /**
+     * Convierte un objeto de tipo UsuarioVO a UsuarioDTO.
+     * 
+     * @param usuario El usuario a convertir.
+     * @return El usuario convertido a DTO.
+     */
+    @Mapping(source = "rol.nombre", target = "rol")
     UsuarioDTO toUsuarioDTO(UsuarioVO usuario);
 
+    /**
+     * Convierte un objeto de tipo UsuarioDTO a UsuarioVO.
+     * 
+     * @param usuarioDTO El DTO de usuario a convertir.
+     * @return El DTO de usuario convertido a entidad.
+     */
     @Mapping(target = "authorities", ignore = true)
     UsuarioVO toUsuario(UsuarioDTO usuarioDTO);
 
-    default String rolToNombre(RolVO rol) {
-        return rol.getNombre().toString();
-    }
-
-    default RolVO nombreToRol(NombreRol nombre) {
-        return servicioRol.findByNombreRol(nombre).orElse(null);
-    }
 }
+
