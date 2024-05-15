89ba19e2a006a97b58bf2e4ec7dfec43a01e77be
Maria Rosete <marlarosete89@gmail.com>
2024-05-15 23:53:38 +0200

    mapperrr

--- pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/servicios/UsuarioMapper.java
+++ pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/servicios/UsuarioMapper.java
@@ -5,33 +5,22 @@ import org.mapstruct.Mapping;
 
 import com.dawes.pdaw_Maria_Rosete.dto.UsuarioDTO;
 import com.dawes.pdaw_Maria_Rosete.modelo.UsuarioVO;
+import com.dawes.pdaw_Maria_Rosete.modelo.RolVO;
 
-/**
- * Interfaz que define métodos para mapear entre objetos DTO y entidades de Usuario.
- * 
- * @author Maria Rosete
- */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ServicioRol.class) // Indicamos que UsuarioMapper usa ServicioRol
 public interface UsuarioMapper {
 
-    /**
-     * Convierte un objeto de tipo UsuarioVO a UsuarioDTO.
-     * 
-     * @param usuario El usuario a convertir.
-     * @return El usuario convertido a DTO.
-     */
     @Mapping(source = "rol.nombre", target = "rol")
+    @Mapping(target = "clave", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "token", ignore = true)
     UsuarioDTO toUsuarioDTO(UsuarioVO usuario);
 
-    /**
-     * Convierte un objeto de tipo UsuarioDTO a UsuarioVO.
-     * 
-     * @param usuarioDTO El DTO de usuario a convertir.
-     * @return El DTO de usuario convertido a entidad.
-     */
     @Mapping(target = "authorities", ignore = true)
+    @Mapping(target = "idusuario", ignore = true)
     UsuarioVO toUsuario(UsuarioDTO usuarioDTO);
 
+    // Método abstracto para buscar RolVO por nombre
+    RolVO findByNombreRol(String nombre);
 }
-
 
