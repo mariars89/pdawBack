3ebcf23ea436c1b58e9fbb5d77cf0f8551c39aff
Maria Rosete <marlarosete89@gmail.com>
2024-05-16 19:44:23 +0200

    maria

--- pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/servicios/UsuarioMapper.java
+++ pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/servicios/UsuarioMapper.java
@@ -6,32 +6,13 @@ import org.mapstruct.Mapping;
 import com.dawes.pdaw_Maria_Rosete.dto.UsuarioDTO;
 import com.dawes.pdaw_Maria_Rosete.modelo.UsuarioVO;
 
-/**
- * Interfaz que define métodos para mapear entre objetos DTO y entidades de Usuario.
- * 
- * @autor Maria Rosete
- */
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    /**
-     * Convierte un objeto de tipo UsuarioVO a UsuarioDTO.
-     * 
-     * @param usuario El usuario a convertir.
-     * @return El usuario convertido a DTO.
-     */
     @Mapping(source = "rol.nombre", target = "rol")
     UsuarioDTO toUsuarioDTO(UsuarioVO usuario);
 
-    /**
-     * Convierte un objeto de tipo UsuarioDTO a UsuarioVO.
-     * 
-     * @param usuarioDTO El DTO de usuario a convertir.
-     * @return El DTO de usuario convertido a entidad.
-     */
-    @Mapping(target = "authorities", ignore = true)
-    @Mapping(target = "idusuario", ignore = true)
-    @Mapping(source = "rol", target = "rol")
+    @Mapping(source = "rol", target = "rol.nombre")
+    @Mapping(target = "idusuario", ignore = true) // Ignora el id para operaciones de actualización
     UsuarioVO toUsuario(UsuarioDTO usuarioDTO);
 }
-
