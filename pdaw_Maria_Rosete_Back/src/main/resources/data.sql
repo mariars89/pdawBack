319acb2241fb5409225b9e0086a131e259371cb2
Maria Rosete <marlarosete89@gmail.com>
2024-05-18 13:48:33 +0200

    datasql

---
+++ pdaw_Maria_Rosete_Back/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (nombre) VALUES ('ADMINISTRADOR', 'REGISTRADO');
+INSERT INTO usuarios (idusuario, apellidos, correo, nombre, password, idrol) VALUES ('1', 'Rosete Suárez', 'marla@example.com', 'Maria', '$2a$10$Kt5GE8A0fwGXItm.MXgwoex4XugRvs6HTis9iS1.slHvMVYt2wQYC', '1'),
+
