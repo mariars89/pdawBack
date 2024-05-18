0794b00beb8e77d486ef1252d7d667d79ff4b68e
Maria Rosete <marlarosete89@gmail.com>
2024-05-18 14:12:53 +0200

    cambios

--- pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/config/ConfiguracionSeguridad.java
+++ pdaw_Maria_Rosete_Back/src/main/java/com/dawes/pdaw_Maria_Rosete/config/ConfiguracionSeguridad.java
@@ -1,5 +1,7 @@
 package com.dawes.pdaw_Maria_Rosete.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.dawes.pdaw_Maria_Rosete.jwt.JwtAccesoDenegadoError;
 import com.dawes.pdaw_Maria_Rosete.jwt.JwtAutenticacionError;
@@ -90,5 +95,17 @@ public class ConfiguracionSeguridad {
         http.addFilterBefore(jwtFiltroPeticiones(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- pdaw_Maria_Rosete_Back/src/main/resources/data.sql
+++ pdaw_Maria_Rosete_Back/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO roles (nombre) VALUES ('ADMINISTRADOR', 'REGISTRADO');
-INSERT INTO usuarios (idusuario, apellidos, correo, nombre, password, idrol) VALUES ('1', 'Rosete Suárez', 'marla@example.com', 'Maria', '$2a$10$Kt5GE8A0fwGXItm.MXgwoex4XugRvs6HTis9iS1.slHvMVYt2wQYC', '1'),
+INSERT INTO usuarios (idusuario, apellidos, correo, nombre, password, idrol) VALUES ('1', 'Rosete Suárez', 'marla@example.com', 'Maria', '$2a$10$Kt5GE8A0fwGXItm.MXgwoex4XugRvs6HTis9iS1.slHvMVYt2wQYC', '1');
 
